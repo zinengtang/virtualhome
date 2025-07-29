cdf5f481c498011f62f7f08649e8716f509a19d1
Zineng Tang <82334223+zinengtang@users.noreply.github.com>
2025-07-30 04:57:27 +0800

    Update __init__.py

--- virtualhome/__init__.py
+++ virtualhome/__init__.py
@@ -2,10 +2,10 @@ import glob
 import sys
 from sys import platform
 
-# Needs to be fixed!
-original_path = sys.path[5]
-new_path = original_path + '/virtualhome/simulation'
-sys.path.append(new_path)
+import importlib.util, pathlib
+import virtualhome
+SIM_PATH = str(pathlib.Path(virtualhome.__file__).parent / 'simulation')
+sys.path.append(SIM_PATH)
 
 from unity_simulator.comm_unity import UnityCommunication
 from unity_simulator import utils_viz
